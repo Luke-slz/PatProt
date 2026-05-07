854ca3c7d2f829ddc854a2021e081029a79a057b
Luke Schulz <luke@famschu.com>
2026-05-07 14:00:08 +0200

    Initial Commit

---
+++ PatProt/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  PatProt
+//
+//  Created by Luke Schulz on 07.05.26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ PatProtTests/PatProtTests.swift
@@ -0,0 +1,19 @@
+//
+//  PatProtTests.swift
+//  PatProtTests
+//
+//  Created by Luke Schulz on 07.05.26.
+//
+
+import Testing
+@testable import PatProt
+
+struct PatProtTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
